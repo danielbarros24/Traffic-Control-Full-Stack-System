571cb03afe969754ab025138b64e443bb3d5e5a3
Daniel Barros <daniel.barros_99@hotmail.com>
2022-04-15 16:39:15 +0100

    frontend/dashboard: added hour picker; /settings: added automation button

--- vue-flask/frontend/src/views/Dashboard.vue
+++ vue-flask/frontend/src/views/Dashboard.vue
@@ -1,33 +1,36 @@
 <template>
   <div>
-      <v-app-bar color="transparent" dark elevation="0">
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-app-bar color="transparent" dark elevation="0">
+      <v-img
+        max-height="35"
+        max-width="35"
+        src="../assets/logo_simple.png"
+      ></v-img>
+      <v-toolbar-title class="ml-4">Dashboard</v-toolbar-title>
 
-        <v-toolbar-title>Dashboard</v-toolbar-title>
+      <v-spacer></v-spacer>
 
-        <v-spacer></v-spacer>
+      <v-menu transition="slide-y-transition" offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon v-bind="attrs" v-on="on">
+            <v-icon>mdi-account</v-icon>
+          </v-btn>
+        </template>
 
-        <v-menu transition="slide-y-transition" offset-y>
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn icon v-bind="attrs" v-on="on">
-              <v-icon>mdi-account</v-icon>
-            </v-btn>
-          </template>
-
-          <v-list>
-            <v-list-item
-              v-for="(item, index) in items"
-              :key="index"
-              @click="handleClick(index)"
-            >
-              <v-list-item-icon>
-                <v-icon v-text="item.icon"></v-icon>
-              </v-list-item-icon>
-              <v-list-item-title>{{ item.title }}</v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu>
-      </v-app-bar>
+        <v-list>
+          <v-list-item
+            v-for="(item, index) in items"
+            :key="index"
+            @click="handleClick(index)"
+          >
+            <v-list-item-icon>
+              <v-icon v-text="item.icon"></v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
     <v-row no-gutters>
       <v-col cols="1" sm="3" md="2">
         <v-card
@@ -39,76 +42,126 @@
           <v-card-text>
             <h2 class="text-h6 text--primary">Location</h2>
             <v-radio-group v-model="Location" mandatory>
-              <v-radio
-                label="Camera 1"
-                value="cam-1"
-                color="primary"
-              ></v-radio>
-              <v-radio
-                label="Camera 2"
-                value="cam-2"
-                color="primary"
-              ></v-radio>
+              <v-radio label="Camera 1" value="Cam 1" color="primary"></v-radio>
+              <v-radio label="Camera 2" value="Cam 2" color="primary"></v-radio>
             </v-radio-group>
 
             <div class="my-12">
               <h2 class="text-h6 text--primary">Time</h2>
 
               <v-menu
-              v-model="menu2"
-              :close-on-content-click="false"
-              :nudge-right="40"
-              transition="scale-transition"
-              offset-y
-              min-width="auto"
-            >
-              <template v-slot:activator="{ on, attrs }">
-                <v-text-field
-                  v-model="dateRangeText"
-                  label="Select date range"
-                  prepend-icon="mdi-calendar"
-                  readonly
-                  clearable
-                  v-bind="attrs"
-                  v-on="on"
-                ></v-text-field>
-              </template>
-              <v-date-picker
-                v-model="dates"
-                range
-              ></v-date-picker>
-            </v-menu>
+                v-model="menu2"
+                :close-on-content-click="false"
+                :nudge-right="40"
+                transition="scale-transition"
+                offset-y
+                min-width="auto"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field
+                    v-model="dateRangeText"
+                    label="Select date range"
+                    prepend-icon="mdi-calendar"
+                    readonly
+                    v-bind="attrs"
+                    v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-date-picker v-model="dates" range></v-date-picker>
+              </v-menu>
+
+              <v-menu
+                ref="menu1"
+                v-model="menuStart"
+                :close-on-content-click="false"
+                :nudge-right="40"
+                :return-value.sync="time1"
+                transition="scale-transition"
+                offset-y
+                max-width="290px"
+                min-width="290px"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field
+                    v-model="time1"
+                    label="Start Time"
+                    prepend-icon="mdi-clock-time-four-outline"
+                    readonly
+                    v-bind="attrs"
+                    v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-time-picker
+                  v-if="menuStart"
+                  v-model="time1"
+                  format="24h"
+                  scrollable
+                  full-width
+                  @click:minute="$refs.menu1.save(time1)"
+                ></v-time-picker>
+              </v-menu>
+
+              <v-menu
+                ref="menu2"
+                v-model="menuEnd"
+                :close-on-content-click="false"
+                :nudge-right="40"
+                :return-value.sync="time2"
+                transition="scale-transition"
+                offset-y
+                max-width="290px"
+                min-width="290px"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field
+                    v-model="time2"
+                    label="End Time"
+                    prepend-icon="mdi-clock-time-four-outline"
+                    readonly
+                    v-bind="attrs"
+                    v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-time-picker
+                  v-if="menuEnd"
+                  v-model="time2"
+                  format="24h"
+                  scrollable
+                  full-width
+                  @click:minute="$refs.menu2.save(time2)"
+                ></v-time-picker>
+              </v-menu>
             </div>
-            
 
             <h2 class="text-h6 text--primary">Indicators</h2>
             <v-checkbox
               v-on:click="VehicleCountsGraph = !VehicleCountsGraph"
               label="Vehicle Counts"
+              value="Counts"
               hide-details
             ></v-checkbox>
             <v-checkbox
               v-on:click="NumberOfStayGraph = !NumberOfStayGraph"
               label="Number of Stay"
-              value="counts"
+              value="Stay"
               hide-details
             ></v-checkbox>
             <v-checkbox
               v-on:click="LenghtOfStayGraph = !LenghtOfStayGraph"
               label="Lenght of Stay"
-              value="counts"
+              value="length"
               hide-details
             ></v-checkbox>
             <v-checkbox
               v-on:click="TrafficFlowAnalysisGraph = !TrafficFlowAnalysisGraph"
               label="Traffic flow"
-              value="counts"
+              value="Flow"
               hide-details
             ></v-checkbox>
             <v-checkbox
               v-on:click="PedestrianFlowGraph = !PedestrianFlowGraph"
               label="Pedestrian Flow"
-              value="counts"
+              value="Pedestrian"
               hide-details
             ></v-checkbox>
           </v-card-text>
@@ -213,6 +266,13 @@ export default {
 
     dates: [],
 
+    Location: "",
+
+    time1: null,
+    time2: null,
+    menuStart: false,
+    menuEnd: false,
+
     VehicleCountsGraph: false,
     TrafficFlowAnalysisGraph: false,
     NumberOfStayGraph: false,
@@ -226,9 +286,9 @@ export default {
     },
   },
   computed: {
-      dateRangeText () {
-        return this.dates.join(' ~ ')
-      },
+    dateRangeText() {
+      return this.dates.join(" ~ ");
     },
+  },
 };
 </script>
--- vue-flask/frontend/src/views/Settings.vue
+++ vue-flask/frontend/src/views/Settings.vue
@@ -1,13 +1,83 @@
 <template>
-  
+  <div>
+    <v-app-bar color="transparent" dark elevation="0">
+      <v-img
+        max-height="35"
+        max-width="35"
+        src="../assets/logo_simple.png"
+        @click="clickLogo()"
+      ></v-img>
+      <v-toolbar-title class="ml-4">Settings</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-menu transition="slide-y-transition" offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon v-bind="attrs" v-on="on">
+            <v-icon>mdi-account</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+            v-for="(item, index) in items"
+            :key="index"
+            @click="handleClick(index)"
+          >
+            <v-list-item-icon>
+              <v-icon v-text="item.icon"></v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+
+    <v-col>
+      <v-btn
+        color="secondary"
+        depressed
+        elevation="4"
+        large
+        raised
+        rounded
+        mx-auto
+      > <v-icon class="mr-3">mdi-plus</v-icon>Create automation
+      </v-btn>
+    </v-col>
+  </div>
 </template>
 
 <script>
 export default {
+  data: () => ({
+    items: [
+      {
+        title: "Logout",
+        icon: "mdi-logout",
+        click() {
+          console.log("logout");
+          this.$router.push("/");
+        },
+      },
+      {
+        title: "Dashboard",
+        icon: "mdi-view-dashboard",
+        click() {
+          console.log("dashboard");
+          this.$router.push("dashboard");
+        },
+      },
+    ],
+  }),
 
-}
+  methods: {
+    handleClick(index) {
+      this.items[index].click.call(this);
+    },
+    clickLogo() {
+      this.$router.push("dashboard");
+    },
+  },
+};
 </script>
-
-<style>
-
-</style>
