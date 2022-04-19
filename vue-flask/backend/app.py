24ffcda9f1be509e5818f469b3dd3c875ab10684
Daniel Barros <daniel.barros_99@hotmail.com>
2022-04-19 17:11:20 +0100

    app.py: added flask comunication with database

--- vue-flask/backend/app.py
+++ vue-flask/backend/app.py
@@ -1,15 +1,25 @@
-from flask import Flask, request
+from asyncio import tasks
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+from tinydb import TinyDB, Query
 
 app = Flask(__name__)
 Cors = CORS(app)
+
+# Database
+db = TinyDB('db.json')
+query = Query()
 
 CORS(app, resources={r'/*': {'origins':
  '*'}}, CORS_SUPPORTS_CREDENTIALS=True)
 
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.post("/dataentry")
+@app.route('/db', methods=['GET'])
+def load():
+    return jsonify(db.search(query.Data.Count == 24))
+
+@app.post("/data")
 def submitData():  
     response_object = {'status': 'success'} 
     
@@ -20,7 +30,26 @@ def submitData():
     print(username) 
     print(password)     
 
-    response_object['message'] = 'Data added!'
+    response_object['message'] = username, password
 
     return response_object
 
+@app.post("/dash")
+def ChartData():
+    response = {'status': 'success'} 
+
+    dash_data = request.get_json()
+
+    cam = dash_data.get('cam'),
+    time = dash_data.get('time'),
+    task = dash_data.get('task'),
+
+    print("Responded!")
+
+    return {
+        "cam": cam,
+        "time": time,
+        "task": task,
+    }
+        
+    
