1df9d10eb7983fb97437874dfb9ca43b71ce64ab
Daniel Barros <daniel.barros_99@hotmail.com>
2022-06-21 10:55:43 +0100

    frontend build update

--- vue-flask/frontend/src/api/index.js
+++ vue-flask/frontend/src/api/index.js
@@ -9,13 +9,13 @@ const urlRasp = "192.168.1.216:8080"
 
 
 export function authenticate (userData) {  
-    const response = await fetch(`http://${urlDesktop}/login`, {
+    /*const response = await fetch(`http://${urlDesktop}/login`, {
         method: "POST",
         headers: {
           'Accept': 'application/json',
           'Content-Type': 'application/json;charset=utf-8'
         },
         body: JSON.stringify(userData),
-      })
+      })*/
 }
   
